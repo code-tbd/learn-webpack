d3427d37efafa1a5987e2cc2bf3a1771db57bf82
code-tbd <ukmukk@163.com>
2022-01-27 17:51:41 +0800

    2022/01/27

--- 05_webpack模块化原理/src/main.js
+++ 05_webpack模块化原理/src/main.js
@@ -1,7 +1,5 @@
 import { sum, mul } from './js/math'
 const { dateFormate, priceFormate } = require('./js/formate')
-import './js/test'
-import './js/component'
 
 console.log(sum(20, 30))
 console.log(mul(20, 30))
--- 05_webpack模块化原理/webpack.config.js
+++ 05_webpack模块化原理/webpack.config.js
@@ -3,87 +3,18 @@ const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 // 打包生成html
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-// 定义全局变量
-const { DefinePlugin } = require('webpack')
-// 拷贝文件
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: './src/main.js',
   output: {
     filename: 'js/bundle.js',
     path: path.resolve(__dirname, './build')
-    // assetModuleFilename: 'img/[name].[hash:6][ext]'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
-          },
-          'postcss-loader'
-        ]
-      },
-      {
-        test: /\.less$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 2
-            }
-          },
-          'postcss-loader',
-          'less-loader'
-        ]
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/,
-        type: 'asset',
-        generator: {
-          filename: 'img/[name].[hash:6][ext]'
-        },
-        parser: {
-          dataUrlCondition: {
-            // 小于50字节转为base64，大于50字节保留文件
-            maxSize: 50 * 1024
-          }
-        }
-      },
-      {
-        test: /\.(ttf|eot|woff2?)$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'font/[name].[hash:6][ext]'
-        }
-      }
-    ]
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'my test webpack',
-      template: './public/index.html'
-    }),
-    new DefinePlugin({
-      BASE_URL: '"./"'
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: 'public',
-          globOptions: {
-            ignore: ['**/index.html', '**/.DS_Store']
-          }
-        }
-      ]
+      title: 'my test webpack'
     })
   ]
 }
